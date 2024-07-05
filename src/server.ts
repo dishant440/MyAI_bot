c98530d6b26bc40a9bceba7db2727d033dc0b84b
Dishant <dishantd442442@gmail.com>
2024-07-05 11:10:28 +0530

    first commit setup the project

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const telegram_bot = require('node-telegram-bot-api');
+
+const bot_token = "6853668850:AAGMPUf21T3-J6GK5SiAeQKvypMUjprV4BY"
+
+const bot = new telegram_bot(bot_token, { polling: true })
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+
+    const chatId = msg.chat.id;
+    const resp = match[1];
+    bot.sendMessage(chatId, resp);
+});
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+  
+   bot.sendMessage(chatId, 'Received your message');
+  });
